fc36c5ab9c4a293dd653826cc035c39b9ae5f419
3shin44 <3shin44@gmail.com>
2023-01-12 21:56:02 +0800

    update enviroment

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # 常用元件與函式庫
 
 ## 預覽連結
-[連結](https://3shin44.github.io/component-library/dist/#/notification-management)
+[連結](https://3shin44.github.io/component-library/dist/)
 
-https://3shin44.github.io/component-library/dist/#/notification-management
+https://3shin44.github.io/component-library/dist/
 
 
 ## 說明
 
 本專案為常用元件與函式庫，僅供查詢使用
+
+
+## 重要套件與版本
+
+- bootstrap: ^5.2.3 / popperjs: ^2.11.6
+- fortawesome: ^6.2.1
+- element-plus: ^2.2.26
+- vue: ^3.2.13
+- vue-router: ^4.0.3
+- vuex: ^3.2.13
+- vue: ^4.0.0
--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,97 @@
   <div class="p-2 info-bg-dark">
     <h1>
       <router-link to="/" class="text-white text-decoration-none">常用元件與函式</router-link>
-      
+
     </h1>
   </div>
 
   <div class="d-flex">
-    <nav class="d-flex flex-column col-2 p-2 min-vh-100 info-shadow">
-      <router-link to="customer-management-preview" class="btn btn-light" :class="{ 'active' : $route.name == 'customer-management-preview' }" >客戶管理</router-link>
-      <router-link to="query-contract" class="btn btn-light" :class="{ 'active' : $route.name == 'query-contract' }" >合約查詢</router-link>
-      <router-link to="notification-management" class="btn btn-light" :class="{ 'active' : $route.name == 'notification-management' }" >通知管理</router-link>
+    <nav class="d-flex flex-column col-2 p-2 info-shadow nav-height">
+      <!-- render router list by "renderList" -->
+      <div v-for="(item, index) in renderList" :key="index">
+        <div v-if="item.type == 'title'">
+          <p class="fs-6 fw-bolder"><font-awesome-icon icon="fa-solid fa-circle-dot" /> {{item.pageName}}</p>
+        </div>
+        <div v-else>
+          <router-link :to="item.routerLink" class="btn btn-light w-100"
+            :class="{ 'active' : $route.name == item.routerLink }">
+            {{item.pageName}}
+          </router-link>
+        </div>
+      </div>
+
       <hr>
-      ©Copyleft
+      © Copyleft
     </nav>
     <div class="col-10 p-2">
+      <!-- default page content -->
+      <div v-if="$route.name==null" class="w-100 px-3">
+        <p class="text-center fs-3"> - the task is failed successfully -</p>
+        <br>
+        <p class="fw-bold">說明</p>
+        <p>本專案為常用元件與函式庫，僅供查詢使用</p>
+        <br>
+        <p class="fw-bold">重要套件與版本</p>
+        <ul>
+          <li>bootstrap: ^5.2.3 / popperjs: ^2.11.6</li>
+          <li>fortawesome: ^6.2.1</li>
+          <li>element-plus: ^2.2.26</li>
+          <li>vue: ^3.2.13</li>
+          <li>vue-router: ^4.0.3</li>
+          <li>vuex: ^3.2.13</li>
+          <li>vue: ^4.0.0</li>
+        </ul>
+      </div>
       <router-view />
     </div>
   </div>
 
-
-
-
 </template>
+
+<script>
+  import { reactive, toRefs } from 'vue'
+
+  export default {
+    name: 'App',
+
+    setup() {
+      const data = reactive({
+        // 頁面清單
+        renderList: [
+          {
+            type: "title",
+            routerLink: "",
+            pageName: "元件類"
+          },
+          {
+            type: "link",
+            routerLink: "customer-management-preview",
+            pageName: "客戶管理"
+          },
+          {
+            type: "link",
+            routerLink: "query-contract",
+            pageName: "合約查詢"
+          },
+          {
+            type: "title",
+            routerLink: "",
+            pageName: "語法類"
+          },
+          {
+            type: "link",
+            routerLink: "notification-management",
+            pageName: "通知管理"
+          },
+        ]
+      })
+
+      return {
+        ...toRefs(data),
+      }
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
 </style>
